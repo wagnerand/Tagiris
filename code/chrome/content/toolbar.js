463315a83b5252865d1d558e5ebc861dcc86a31e
Andreas Wagner <mail@andreaswagner.org>
2012-10-23 10:07:52 +0200

    Fixed broken tabbar because of leaking variables

--- code/chrome/content/toolbar.js
+++ code/chrome/content/toolbar.js
@@ -3,6 +3,8 @@
  * Copyright (C) 2010 Andreas Wagner
  */
 
+// See https://bugzilla.mozilla.org/show_bug.cgi?id=550175
+
 if (!Tagiris) {
     var Tagiris = {
 
@@ -13,33 +15,35 @@ if (!Tagiris) {
         toolbar : null,
 
         init : function () {
-            searchBox = document.getElementById("TagirisSearchBox");
-            toolbar = Object.create(PlacesToolbar.prototype);
-
-            var popup = document.getElementById("editBookmarkPanel");
+            let popup = document.getElementById("editBookmarkPanel");
             popup.addEventListener('popupshown', function(event) {
                 Tagiris.updatePanel(event);
             }, false);
 
-            toolbar._viewElt = document.getElementById("TagirisToolbar");
-            toolbar._rootElt = document.getElementById("TagirisToolbarItems");
-            toolbar._dropIndicator = document.getElementById("TagirisToolbarDropIndicator");
-            toolbar._chevron = document.getElementById("TagirisChevron");
-            toolbar._chevronPopup = document.getElementById("TagirisChevronPopup");
-            toolbar._viewElt._placesView = toolbar;
-            toolbar._addEventListeners(toolbar._viewElt, toolbar._cbEvents, false);
-            toolbar._addEventListeners(toolbar._rootElt, ["popupshowing", "popuphidden"], true);
-            toolbar._addEventListeners(toolbar._rootElt, ["overflow", "underflow"], true);
-            toolbar._addEventListeners(window, ["resize", "unload"], false);
+            this.searchBox = document.getElementById("TagirisSearchBox");
+            this.toolbar = Object.create(PlacesToolbar.prototype);
 
-            searchBox.value = Tagiris.prefs.getCharPref("contextTags");
-            Tagiris.showContextResults();
+            this.toolbar._viewElt = document.getElementById("TagirisToolbar");
+            this.toolbar._rootElt = document.getElementById("TagirisToolbarItems");
+            this.toolbar._dropIndicator = document.getElementById("TagirisToolbarDropIndicator");
+            this.toolbar._chevron = document.getElementById("TagirisChevron");
+            this.toolbar._chevronPopup = document.getElementById("TagirisChevronPopup");
+            this.toolbar._viewElt._placesView = this.toolbar;
+            this.toolbar._addEventListeners(this.toolbar._viewElt, this.toolbar._cbEvents, false);
+            this.toolbar._addEventListeners(this.toolbar._rootElt, ["popupshowing", "popuphidden"], true);
+            this.toolbar._addEventListeners(this.toolbar._rootElt, ["overflow", "underflow"], true);
+            this.toolbar._addEventListeners(window, ["resize", "unload"], false);
 
-            toolbar._viewElt.addEventListener("dragstart", function(event) { event.stopPropagation(); }, true);
-            toolbar._viewElt.addEventListener("dragover", function(event) { event.stopPropagation(); }, true);
-            toolbar._viewElt.addEventListener("dragexit", function(event) { event.stopPropagation(); }, true);
-            toolbar._viewElt.addEventListener("dragend", function(event) { event.stopPropagation(); }, true);
-            toolbar._viewElt.addEventListener("drop", function(event) { event.stopPropagation(); }, true);
+            PlacesViewBase.call(this.toolbar, null);
+
+            this.searchBox.value = this.prefs.getCharPref("contextTags");
+            this.showContextResults.call(this);
+
+            this.toolbar._viewElt.addEventListener("dragstart", function(event) { event.stopPropagation(); }, true);
+            this.toolbar._viewElt.addEventListener("dragover", function(event) { event.stopPropagation(); }, true);
+            this.toolbar._viewElt.addEventListener("dragexit", function(event) { event.stopPropagation(); }, true);
+            this.toolbar._viewElt.addEventListener("dragend", function(event) { event.stopPropagation(); }, true);
+            this.toolbar._viewElt.addEventListener("drop", function(event) { event.stopPropagation(); }, true);
         },
 
         updatePanel : function(event) {
@@ -47,7 +51,7 @@ if (!Tagiris) {
             if (event.target != document.getElementById("editBookmarkPanel"))
                 return;
             if (tagsField.value != "") {
-                var searchTagString = searchBox.value;
+                var searchTagString = Tagiris.searchBox.value;
                 var oldTagString = tagsField.value;
                 var newTagString = searchTagString + "," + oldTagString + ",";
                 var tempTags = newTagString.replace(/,\s+/g, ",").replace(/\s+,/g, ",").split(",");
@@ -60,12 +64,12 @@ if (!Tagiris) {
                 }
                 tagsField.value = newTags.join(", ");
             } else {
-                tagsField.value = searchBox.value.trim();
+                tagsField.value = Tagiris.searchBox.value.trim();
             }
         },
 
         showContextResults : function() {
-            var searchText = searchBox.value.replace(/\s+/g, " ").trim();
+            var searchText = this.searchBox.value.replace(/\s+/g, " ").trim();
             if (searchText.lastIndexOf(",") == searchText.length - 1) {
                 searchText = searchText.substring(0, searchText.length - 1);
             }
@@ -73,20 +77,25 @@ if (!Tagiris) {
             for (var i = 0; i < tags.length; i++) {
                 tags[i] = tags[i].trim();
             }
-            Tagiris.prefs.setCharPref("contextTags", searchText);
+            this.prefs.setCharPref("contextTags", searchText);
 
             if (searchText.length > 0) {
-                var query = Tagiris.historyService.getNewQuery();
-                var options = Tagiris.historyService.getNewQueryOptions();
+                var query = this.historyService.getNewQuery();
+                var options = this.historyService.getNewQueryOptions();
 
                 query.tags = tags;
                 options.excludeQueries = true;
                 options.queryType = 1;
                 options.sortingMode = 4;
 
-                PlacesViewBase.call(toolbar, Tagiris.historyService.queriesToQueryString([query], 1, options));
+                PlacesViewBase.call(this.toolbar, this.historyService.queriesToQueryString([query], 1, options));
+            } else {
+                while (this.toolbar._rootElt.hasChildNodes()) {
+                    this.toolbar._rootElt.removeChild(this.toolbar._rootElt.firstChild);
+                }
             }
         }
+        
     };
 }
 
